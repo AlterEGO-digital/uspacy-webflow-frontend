ccd45b8a08e94f0665ca4035e9490b6bc4a5ecbd
Вітас Пірожко <vitaspirozko@Vitass-Air.Dlink>
2023-09-14 19:39:18 +0300

    is stage fix

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,3 @@
 export const isDev = process.env.NODE_ENV === 'development';
 
-export const isStage = window.location.pathname.includes('staging.uspacy.tech');
+export const isStage = window.location.host.includes('staging.uspacy.tech');
